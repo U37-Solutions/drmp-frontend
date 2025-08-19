be122e423e129d9630f7b9775d2b6087ee82ffb9
Oleksandr Smal <oleksandr.smal@match2one.com>
2025-08-19 10:09:53 +0300

    fix: Bulk fix mobile UI issues

--- src/features/map/components/SuppliersMap/SuppliersMap.tsx
+++ src/features/map/components/SuppliersMap/SuppliersMap.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { MarkerClusterer } from '@googlemaps/markerclusterer';
 import { useMap } from '@vis.gl/react-google-maps';
 import { Card, Flex } from 'antd';
 import React, { useMemo } from 'react';
@@ -18,7 +19,21 @@ const SuppliersMap = () => {
   const { mapPoints, filters, setFilters } = useMapData();
   const [viewMode, setViewMode] = React.useState<ViewMode>(ViewMode.Map);
 
-  const renderedMap = useMemo(() => <LocationMap mapInstance={map} data={mapPoints} />, [map, mapPoints]);
+  const markerClusterer = useMemo(() => {
+    if (!map) return null;
+
+    return new MarkerClusterer({ map });
+  }, [map]);
+
+  const handleViewModeChange = (mode: ViewMode) => {
+    setViewMode(mode);
+    markerClusterer?.clearMarkers();
+  };
+
+  const renderedMap = useMemo(
+    () => <LocationMap mapInstance={map} markerClusterer={markerClusterer} data={mapPoints} />,
+    [map, mapPoints, markerClusterer],
+  );
   const renderedList = useMemo(() => <SuppliersList data={mapPoints} />, [mapPoints]);
 
   return (
@@ -27,7 +42,7 @@ const SuppliersMap = () => {
         <Filters filters={filters} setFilters={setFilters} />
       </Flex>
       <Card className={styles.mapCard}>
-        <ViewToggler viewMode={viewMode} onChange={setViewMode} />
+        <ViewToggler viewMode={viewMode} onChange={handleViewModeChange} />
         {viewMode === ViewMode.Map ? renderedMap : renderedList}
       </Card>
     </Flex>
